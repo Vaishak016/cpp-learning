bd768c44b9c5af335fb2e30d1fef3f8a20a184a3
Vaishak016 <vaishak9138@gmail.com>
2026-02-07 01:27:18 +0530

    add array - to check if the array is sorted

---
+++ arrays_using_functions/sorted_array.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+bool sortedarray(vector<int> & arr)
+{
+    int n = arr.size();
+    int i =0;
+    int j = i+1;
+    while(j<n)
+    {
+        if(arr[i]>arr[j])
+        {
+            return false;        
+        }
+        i++;
+        j++; 
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of elements you want in the array:" << endl;
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements:" << endl;
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bool ans = sortedarray(arr);
+    if(ans)
+    {
+        cout << "The array is sorted.";
+    }
+    else
+    {
+        cout << "The array is not sorted.";
+    }
+
+
+    return 0;
+}
