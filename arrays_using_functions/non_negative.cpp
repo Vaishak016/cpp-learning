d6fa823e5001aa30337886c09d0775802f216699
Vaishak016 <vaishak9138@gmail.com>
2026-02-11 18:12:34 +0530

    program to replace all the negative integers in the array using functions

--- arrays_using_functions/non_negative.cpp
+++ arrays_using_functions/non_negative.cpp
@@ -11,26 +11,26 @@ void nonnegative(vector<int> & arr)
             arr[i] = 0;
         }
     }
-
-    for(int i=0; i<n; i++)
-    {
-        cout << arr[i];
-    }
 }
 int main()
 {
     int n;
-    cout << "How many elements do you want in the array?" << endl;
+    cout << "Enter the number of elements you want in the array?" << endl;
     cin >> n;
 
-    int i;
     vector<int> arr(n);
+    int i;
     cout << "Enter the elements:" << endl;
     for(i=0; i<n; i++)
     {
         cin >> arr[i];
     }
-
+    
     nonnegative(arr);
+    cout << "After changing the non-negative elements in the array:" << endl;
+    for(i=0; i<n; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
