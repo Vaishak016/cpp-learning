eaa087fbed27b3ae9ae63e44b5c5eaf8128c6cdc
Vaishak016 <vaishak9138@gmail.com>
2026-02-12 00:39:31 +0530

    program to copy the given array

---
+++ arrays_using_functions/copy_arr.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void copyarray(const vector<int> & arr, vector<int> & copyarr)
+{
+    int n = arr.size();
+    
+    for(int i=0; i<arr.size(); i++)
+    {
+        copyarr.push_back(arr[i]);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the number of elements you want in the array?" << endl;
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements:" << endl;
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> copyarr;
+    copyarray(arr, copyarr);
+    
+    for(int i=0; i<copyarr.size(); i++)
+    {
+        cout << copyarr[i] << " ";
+    }
+    return 0;
+}
